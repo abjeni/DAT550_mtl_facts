8191c85a5214e27bd0bc644d177d6dd59aba3ab0
asbjorn <asbjorn.salhus@gmail.com>
2024-04-16 19:02:57 +0200

    parsing stance

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 
-import pandas
+import pandas as pd
+import json
 
-english_train_checkworthy = pandas.read_csv("data/CT24_checkworthy_english/CT24_checkworthy_english_train.tsv", sep='\t')
-print(english_train_checkworthy)
+train_checkworthy = pd.read_csv("data/CT24_checkworthy_english/CT24_checkworthy_english_train.tsv", sep='\t')
+print(train_checkworthy)
 
+with open("data/English_train.json") as json_file:
+    json_strs = json_file.readlines()
+
+json_datas = [json.loads(json_str) for json_str in json_strs]
+
+train_stance = pd.DataFrame.from_dict(json_datas)
+print(train_stance)
+
+#train_stance = pd.read_json('data/English_train.json')
+#print(train_stance)
 
 """
 with open("data/CT24_checkworthy_english/CT24_checkworthy_english_train.tsv") as train_file:
