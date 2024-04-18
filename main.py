f766ade8bdc40a1172b620a658a3b0021a8a1550
asbjorn <asbjorn.salhus@gmail.com>
2024-04-18 23:22:45 +0200

    oopsie

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from preprocessing import *
 train_claim = pd.read_csv("data/checkworthy/english_train.tsv", sep='\t')
 print(train_claim)
 
-train_stance = pd.read_json("data/stance/English_train.json", orient='records', lines=True)
+train_stance = pd.read_json("data/stance/english_train.json", orient='records', lines=True)
 print(train_stance)
 
 cleanup_dataframe(train_claim)
