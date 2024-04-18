c0cb5834671d009e9188e8e93d7b80581fb0083d
asbjorn <asbjorn.salhus@gmail.com>
2024-04-18 23:22:10 +0200

    full data rename

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import pandas as pd
 import json
 from preprocessing import *
 
-train_claim = pd.read_csv("data/CT24_checkworthy_english/CT24_checkworthy_english_train.tsv", sep='\t')
+train_claim = pd.read_csv("data/checkworthy/english_train.tsv", sep='\t')
 print(train_claim)
 
-train_stance = pd.read_json("data/English_train.json", orient='records', lines=True)
+train_stance = pd.read_json("data/stance/English_train.json", orient='records', lines=True)
 print(train_stance)
 
 cleanup_dataframe(train_claim)
 cleanup_dataframe(train_stance)
 
-train_stance.to_json("data/cleaned_train.json", orient='records', lines=True)
+train_stance.to_json("data/stance/cleaned_train.json", orient='records', lines=True)
